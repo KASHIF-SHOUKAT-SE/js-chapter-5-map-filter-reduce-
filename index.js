8a4c266c615907f7fd65dd9af1298452ffe9b96d
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-28 11:14:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// let arr = [45,76,87,32,47]
+// let a = arr.map((value,index,array)=>{
+//     console.log(value,index,array)
+//     return value +1
+// })
+// console.log(a)
+
+// let arr = [45,76,87,3,7,6,45,35]
+// let a2 = arr.filter((a)=>{
+//     return a<10;
+// })
+// console.log(a2)
+
+
+let arr = [3,5,6,7,8,9,4]
+let newarr3 = arr.reduce((h1,h2)=>{
+    return h1 + h2
+})
+console.log(newarr3)
+
+
+
+
+
+
+
+
+
